a69d121fa0d673465ebf81c0bf7ddd5e5d4a37d0
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2026-06-10 20:54:31 -0400

    Renombra clases

--- app/src/main/java/edu/ucne/james_urena_ap2_p1/data/local/dao/Dao.kt
+++
@@ -1,9 +0,0 @@
-package edu.ucne.james_urena_ap2_p1.data.local.dao
-
-import androidx.room.Dao
-import edu.ucne.james_urena_ap2_p1.data.local.entity.BorrameEntity
-
-@Dao
-interface Dao {
-    suspend fun upsert(entity: BorrameEntity)
-}
--- app/src/main/java/edu/ucne/james_urena_ap2_p1/data/local/database/DataBase.kt
+++
@@ -1,4 +0,0 @@
-package edu.ucne.james_urena_ap2_p1.data.local.database
-
-class DataBase {
-}
