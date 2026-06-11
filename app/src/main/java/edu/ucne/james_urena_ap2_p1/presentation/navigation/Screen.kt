e2c4d9d6cf12bcc9a2552e5b7296301daa9b6c58
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2026-06-10 21:12:27 -0400

    Modifica el Screen.kt

--- app/src/main/java/edu/ucne/james_urena_ap2_p1/presentation/navigation/Screen.kt
+++ app/src/main/java/edu/ucne/james_urena_ap2_p1/presentation/navigation/Screen.kt
@@ -1,10 +1,11 @@
 package edu.ucne.james_urena_ap2_p1.presentation.navigation
 
 import kotlinx.serialization.Serializable
+
 sealed class Screen {
     @Serializable
-    data object BorrameList : Screen()
+    data object AmonestacionList : Screen()
 
     @Serializable
-    data class Borrame(val borrameId: Int) : Screen()
+    data class Amonestacion(val amonestacionId: Int) : Screen()
 }
